ee9b2542467156fa0ef82071c91b7cb2d8711486
sampanna pokhrel <sampanna99@gmail.com>
2017-01-02 12:07:58 -0800

    Add project files.

---
+++ GettingStartedWithAspNetWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GettingStartedWithAspNetWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ GettingStartedWithAspNetWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GettingStartedWithAspNetWebApi.WebApiApplication" Language="C#" %>
